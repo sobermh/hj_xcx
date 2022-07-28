96bb98384bc8894bb5705f085fd501ef7cf90577
hui mao <409788696@qq.com>
2022-07-28 13:23:28 +0800

    使用真机调试

--- case/test_login.py
+++ case/test_login.py
@@ -41,7 +41,7 @@
 #     print(result)
 import minium
 from base.basepage import BasePage
-from page.my_page import MyClass
+from page.my_page import MyPage
 
 # mini = minium.Minium({
 #      "dev_tool_path": "E:\\微信web开发者工具\\cli.bat",
@@ -52,14 +52,14 @@ class TestLogin(minium.MiniTest):
 
     def test_01(self):
         """正确授权登录"""
-        # # 弹出的授权看框没法点
-        # self.page.wait_for(2)
-        # self.page.get_element('.van-icon-user-circle-o').click()  # 点击我的
-        # MyClass(self.page).click_login_out()
-        # BasePage(self.page).click('.authBtn')
-        # self.page.get_element('.authBtn',max_timeout=2).click()  # 点击授权登录
-        # self.page.wait_for(3)  # 当前页面等待3s
-        # # self.assertEqual('/pages/main/index', self.mini.app.current_page.path, msg='登录失败')
-        # # 断言登录后是否跳转到首页，如果断言失败，则返回mgs"登录失败"
-        # self.native.allow_authorize(answer=True)
+        # 弹出的授权看框没法点
+        self.page.wait_for(2)
+        self.page.get_element('.van-icon-user-circle-o').click()  # 点击我的
+        MyPage(self.page).click_login_out()
+        BasePage(self.page).click('.authBtn')
+        self.page.get_element('.authBtn',max_timeout=2).click()  # 点击授权登录
+        self.page.wait_for(3)  # 当前页面等待3s
+        # self.assertEqual('/pages/main/index', self.mini.app.current_page.path, msg='登录失败')
+        # 断言登录后是否跳转到首页，如果断言失败，则返回mgs"登录失败"
+        self.native.allow_authorize(answer=True)
         pass
